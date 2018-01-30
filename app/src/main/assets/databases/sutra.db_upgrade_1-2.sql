1ce50ad62b839a39313317aea1351ff7b0d63c15
jhughes <hughes.james@gmail.com>
2018-01-30 15:15:44 -0800

    Database upgrade to fix truncated sutra II.34

---
+++ app/src/main/assets/databases/sutra.db_upgrade_1-2.sql
@@ -0,0 +1,2 @@
+UPDATE sutras SET phonetic = "Vitarkā hiṁsādayaḥ kṛtakāritānumoditā lobhakrodhamohapūrvakā mṛdumadhyādhimātrā duḥkhājñānānantaphalā iti pratipakṣabhāvanam||34||" WHERE _id IS 85;
+
--- app/src/main/java/sutra/chantapps/jhughes/sutra/DatabaseOpenHelper.java
+++ app/src/main/java/sutra/chantapps/jhughes/sutra/DatabaseOpenHelper.java
@@ -6,7 +6,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "sutra.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
